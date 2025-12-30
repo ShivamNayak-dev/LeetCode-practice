37794f656a099d5c250b4e472e146b2ddbfe80f4
Shivam Nayak <shivamkumarnayak13@gmail.com>
2025-12-30 16:04:04 +0530

    PascalsTrangle

---
+++ Dynamic Programming/PascalsTriangle.java
@@ -0,0 +1,31 @@
+package Dynamic Programming;
+
+import java.util.*;
+
+public class PascalsTriangle {
+    
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            
+            
+            row.add(1);
+            
+            
+            for (int j = 1; j < i; j++) {
+                int val = result.get(i - 1).get(j - 1) + result.get(i - 1).get(j);
+                row.add(val);
+            }
+            
+            if (i > 0) {
+                row.add(1); 
+            }
+            
+            result.add(row);
+        }
+
+        return result;
+    }
+}
