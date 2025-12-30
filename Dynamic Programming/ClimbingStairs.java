d3ca6e14bf59fb2dd40f0d4fefe0b5449e142f4c
Shivam Nayak <shivamkumarnayak13@gmail.com>
2025-12-30 16:00:29 +0530

    ClimingStairsSolution

---
+++ Dynamic Programming/ClimbingStairs.java
@@ -0,0 +1,18 @@
+public class ClimbingStairs {
+
+    public int climbStairs(int n) {
+        
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        int prev1=1, prev2=2;
+
+        for(int i=3; i<=n; i++){
+            int current=prev1+prev2;
+            prev1=prev2;
+            prev2=current;
+        }
+
+        return prev2;
+    }
+}
