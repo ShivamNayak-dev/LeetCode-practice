b1ce66443b2de84e5fe85897730fd8f8dcfc4077
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-01-04 05:28:34 +0530

    Product OfArrayExceptSelf

---
+++ TwoPointerApproches/ProductofArray.java
@@ -0,0 +1,35 @@
+
+
+import java.util.Arrays;
+
+public class ProductofArray {
+    static int[] productExceptSelf(int[] arr) {
+        int n = arr.length;
+        int[] prefProduct = new int[n];
+        int[] suffProduct = new int[n];
+        int[] res = new int[n];
+
+        
+        prefProduct[0] = 1;
+        for (int i = 1; i < n; i++)
+            prefProduct[i] = arr[i - 1] * prefProduct[i - 1];
+
+       
+        suffProduct[n - 1] = 1;
+        for (int j = n - 2; j >= 0; j--)
+            suffProduct[j] = arr[j + 1] * suffProduct[j + 1];
+
+        
+        for (int i = 0; i < n; i++)
+            res[i] = prefProduct[i] * suffProduct[i];
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 3, 5, 6, 2};
+        int[] res = productExceptSelf(arr);
+
+        System.out.println(Arrays.toString(res));
+    }
+}
