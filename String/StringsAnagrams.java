001604ee582a6d64c755d331eb653af4666fed0c
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-01-15 17:04:31 +0530

    Check if two Strings are Anagrams of each other

---
+++ String/StringsAnagrams.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class StringsAnagrams {
+    static boolean areAnagrams(String s1, String s2) {
+        
+        if (s1.length() != s2.length()) return false;
+        
+        
+        char[] s1Array = s1.toCharArray();
+        char[] s2Array = s2.toCharArray();
+        Arrays.sort(s1Array);
+        Arrays.sort(s2Array);
+
+        
+        return Arrays.equals(s1Array, s2Array);
+    }
+
+    public static void main(String[] args) {
+        
+        String s1 = "geeks";
+        String s2 = "kseeg";
+        
+        if(areAnagrams(s1, s2) == true){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+        
+    }
+}
