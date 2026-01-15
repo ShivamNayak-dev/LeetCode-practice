903f18b1fde9a4fe57b217c513c7f7ee7d25c6bc
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-01-15 17:44:10 +0530

    whether it is a palindrome sentence or not

---
+++ String/SentencePalindrome.java
@@ -0,0 +1,38 @@
+
+
+public class SentencePalindrome {
+    static boolean isPalinSent(String s) {
+        int i = 0, j = s.length() - 1;
+
+        
+        while (i < j) {
+
+            
+            if (!Character.isLetterOrDigit(s.charAt(i)))
+                i++;
+
+           
+            else if (!Character.isLetterOrDigit(s.charAt(j)))
+                j--;
+
+           
+            else if (Character.toLowerCase(s.charAt(i)) 
+                    == Character.toLowerCase(s.charAt(j))) {
+                i++;
+                j--;
+            }
+          
+           
+            else
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "Too hot to hoot.";
+        System.out.println(isPalinSent(s) 
+                           ? "true" : "false");
+    }
+}
